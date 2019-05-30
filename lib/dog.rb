30e08009bae9d3af166355366ec13d2f3056897d
garvin tan <garvintan07@gmail.com>
2019-05-30 20:45:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,14 @@ class Dog
      @name 
   end 
   
-  def name=
-     @name
+  def name=(new_name)
+     @name = new_name
   end
   
   def breed 
     @breed 
   end
-  def breed=
-     @breed 
+  def breed=(new_breed)
+     @breed = new_breed
   end
 end
